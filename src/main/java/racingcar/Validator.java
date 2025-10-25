ecbf9510638e2bb78ae32b94794b4c9b91ab73c3
Seokwon Cho <dkf1141@gmail.com>
2025-10-25 23:55:53 +0900

    fix(validator): 경고 문자열 상수화

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -1,6 +1,8 @@
 package racingcar;
 
 public class Validator {
+    private static final String ERROR_UP_NUMBER_ONE = "시도 횟수는 1 이상의 정수여야 합니다.";
+    private static final String  ERROR_NOT_NUMBER = "시도 횟수는 숫자로만 입력해야 합니다.";
 
     private Validator() {}
 
@@ -8,11 +10,11 @@ public class Validator {
         try {
             int number = Integer.parseInt(input);
             if (number <= 0) {
-                throw new IllegalArgumentException("시도 횟수는 1 이상의 정수여야 합니다.");
+                throw new IllegalArgumentException(ERROR_UP_NUMBER_ONE);
             }
             return number;
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("시도 횟수는 숫자로만 입력해야 합니다.");
+            throw new IllegalArgumentException(ERROR_NOT_NUMBER);
         }
     }
 }
--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,6 +1,9 @@
 package racingcar.domain;
 
 public class Car {
+    private static final String ERROR_EMPTY_NAME = "비어있는 값이 입력 되었습니다.";
+    private static final String ERROR_NAME_TOO_LONG = "자동차 이름이 5자를 초과했습니다.";
+
     private final String name;
     private int value = 0;
 
@@ -11,10 +14,10 @@ public class Car {
 
     private void validateName(String name) {
         if(name==null || name.isBlank()) {
-            throw new IllegalArgumentException("비어있는 값이 입력 되었습니다.");
+            throw new IllegalArgumentException(ERROR_EMPTY_NAME);
         }
         if(name.length()>5) {
-            throw new IllegalArgumentException("자동차 이름이 5자를 초과했습니다.");
+            throw new IllegalArgumentException(ERROR_NAME_TOO_LONG);
         }
     }
 
